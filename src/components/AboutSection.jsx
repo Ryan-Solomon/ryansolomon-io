8c013221c71ab73bc83a3c2fb268757dc7d18ecd
Ryan Solomon <ryanjsolomon24@gmail.com>
2020-11-09 14:31:24 -0600

    adds animation to photo

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -3,6 +3,7 @@ import home1 from '../img/home1.png';
 import { About, Description, Hide, Image } from '../styles';
 
 import { motion } from 'framer-motion';
+import { titleAnim, fade, photoAnim } from '../animation';
 
 const AboutSection = () => {
   return (
@@ -10,26 +11,26 @@ const AboutSection = () => {
       <Description>
         <motion.div className='title'>
           <Hide>
-            <motion.h2>We work to make</motion.h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>true.</motion.h2>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio
           veritatis aspernatur praesentium alias maxime iure, vitae adipisci.
           Molestiae, maxime. Placeat.
-        </p>
-        <button>Contact Me</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Me</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt='guy with a camera' />
+        <motion.img variants={photoAnim} src={home1} alt='guy with a camera' />
       </Image>
     </About>
   );
