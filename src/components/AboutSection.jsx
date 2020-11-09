a7e9e7322ff33ad1a5bb4b7ed2ae67da0046e02c
Ryan Solomon <ryanjsolomon24@gmail.com>
2020-11-09 13:58:19 -0600

    adds motion transition in about section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import home1 from '../img/home1.png';
 import { About, Description, Hide, Image } from '../styles';
+
+import { motion } from 'framer-motion';
+
 const AboutSection = () => {
   return (
     <About>
       <Description>
-        <div className='title'>
+        <motion.div
+          variants={container}
+          initial='hidden'
+          animate='show'
+          className='title'
+        >
           <Hide>
-            <h2>We work to make</h2>
+            <motion.h2>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <h2>
+            <motion.h2>
               your <span>dreams</span> come
-            </h2>
+            </motion.h2>
           </Hide>
           <Hide>
-            <h2>true.</h2>
+            <motion.h2>true.</motion.h2>
           </Hide>
-        </div>
+        </motion.div>
         <p>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio
           veritatis aspernatur praesentium alias maxime iure, vitae adipisci.
